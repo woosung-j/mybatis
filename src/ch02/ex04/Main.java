1239b27be160d65c5629bf47459078ad1c739ff2
woosung-j <developer.jws@gmail.com>
2022-07-25 17:00:43 +0900

    update

---
+++ src/ch02/ex04/Main.java
@@ -0,0 +1,16 @@
+package ch02.ex04;
+
+import java.time.LocalDate;
+
+import ch02.domain.User;
+import config.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Map mapper = Configuration.getMapper(Map.class);
+		
+		if(mapper.updateUser(new User(11, "King", LocalDate.of(2027, 7, 12))) > 0) {
+			System.out.println("¼º°ø");
+		}
+	}
+}

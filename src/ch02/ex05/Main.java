895685c5b671d1a750af9b0022d3a116496f10ae
woosung-j <developer.jws@gmail.com>
2022-07-25 17:01:12 +0900

    delete

---
+++ src/ch02/ex05/Main.java
@@ -0,0 +1,13 @@
+package ch02.ex05;
+
+import config.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Map mapper = Configuration.getMapper(Map.class);
+		
+		if(mapper.deleteUser(11) > 0) {
+			System.out.println("¼º°ø");
+		} 
+	}
+}
